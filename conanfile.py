3062f6e2219b43b8a6e10d3d6ebab814ab1dab45
alexFickle <alex.fickle@gmail.com>
2020-02-19 04:08:03 -0600

    implemented conan packaging

--- conanfile.py
+++ conanfile.py
@@ -2,9 +2,17 @@ import typing
 
 from conans import ConanFile, CMake, tools
 
+
 class CLIConanFile(ConanFile):
+    name = "cli"
+    version = "0.0.0"
+    license = "MIT"
+    description = "command line argument parser library"
+    url = "https://github.com/alexFickle/cli"
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake"
+    exports_sources = ("examples/*", "include/*", "test/*", "CMakeLists.txt",
+                       "LICENSE")
 
     build_requires = "gtest/1.8.1@bincrafters/stable"
 
@@ -14,3 +22,10 @@ class CLIConanFile(ConanFile):
         cmake.build()
         if not tools.cross_building(self.settings):
             cmake.test(output_on_failure=True)
+
+    def package_id(self):
+        self.info.header_only()
+
+    def package(self):
+        self.copy("*.hpp", src="include", dst="include")
+        self.copy("LICENSE", src=".", dst=".")
